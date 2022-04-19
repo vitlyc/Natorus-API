cb2f8b0481a2404eba446fd6207bc95369119b40
vitlyc <72764133+vitlyc@users.noreply.github.com>
2022-04-19 20:59:51 +0300

    mongoDb connected

--- app.js
+++ app.js
@@ -1,60 +1,31 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
-require('dotenv');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-console.log(process.env.YYY);
 
 app.use((req, res, next) => {
-  console.log(req.body.name);
+  console.log('Hello from the middleware 👋');
   next();
 });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-app.use(morgan('dev'));
-
-app.get('/api/v1/tours', (req, res) => {
-  console.log(tours);
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((elem) => elem.id === id);
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    res.status(201).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tour: newTour,
-      },
-    });
-  });
-});
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+module.exports = app;
